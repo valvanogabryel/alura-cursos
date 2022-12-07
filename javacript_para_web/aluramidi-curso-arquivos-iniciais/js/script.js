92ac6ad0394c6a29be983fbb109f3d72a3c298a2
valvanogabryel <valvanogabryel@gmail.com>
2022-12-07 10:06:11 -0300

    .

---
+++ javacript_para_web/aluramidi-curso-arquivos-iniciais/js/script.js
@@ -0,0 +1,9 @@
+const soundsBtn = document.querySelectorAll('button');
+
+soundsBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        let btnName = btn.innerHTML.toLowerCase();
+        let audio = document.querySelector(`#som_tecla_${btnName}`);
+        audio.play();
+    });
+});
