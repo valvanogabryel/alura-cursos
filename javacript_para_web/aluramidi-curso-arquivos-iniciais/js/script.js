9c2e9efcde9e120859bd94b29c1e8686c2408e04
valvanogabryel <valvanogabryel@gmail.com>
2022-12-07 12:42:49 -0300

    Update script.js

--- javacript_para_web/aluramidi-curso-arquivos-iniciais/js/script.js
+++ javacript_para_web/aluramidi-curso-arquivos-iniciais/js/script.js
@@ -6,8 +6,21 @@ soundsBtn.forEach(btn => {
         let audio = document.querySelector(`#som_tecla_${btnName}`);
         audio.play();
 
+        btn.addEventListener('keydown', (event) => {
+            if (event.code === 'Enter' || event.code === 'Space') {
+                btn.classList.add('ativa');
+            }
+            /*"code", no objeto "event", refere-se à tecla pressionada, assim como o "key". Porém, no "key", a tecla de espaço é representada como uma string com um espaço vazio (" "), diferetemente da informação que chega no "code", que é o nome da tecla em si ("Space"); Logo, essa condicional poderia também ser escrita da seguinte forma:
+            
+            if (event.key === 'Enter' || event.key === ' ') {
+                btn.classList.add('ativa');
+            }
 
-        btn.classList.add('ativa');
-        setTimeout(() => { btn.classList.remove('ativa') }, 200)
+            No code, por outro lado, uma tecla qualquer, como por exemplo o "w", é descrito como "KeyW", enquanto no "Key", o retorno é a tecla "w";
+            */
+        });
+        btn.addEventListener('keyup', () => {
+            btn.classList.remove('ativa');
+        });
     });
-});
+})
