559df0c423e6515c1bb0c636b58a2325043765fb
valvanogabryel <valvanogabryel@gmail.com>
2022-12-13 18:01:31 -0300

    z

--- css-dispondo-elementos-flexbox-grid/js/index.js
+++ css-dispondo-elementos-flexbox-grid/js/index.js
@@ -2,8 +2,20 @@ const menuBtn = document.querySelector('button.header__menu');
 const sideMenu = document.querySelector('nav.side-menu');
 menuBtn.addEventListener('click', toggleSidebar);
 
+
 function toggleSidebar() {
     sideMenu.classList.toggle('hidden');
     menuBtn.classList.toggle('side-menu__isShowing');
     document.querySelector('main').classList.toggle('defocused');
+    document.querySelector('img.header__logo').classList.toggle('logo-disappear');
+
+    preventScroll();
+}
+
+function preventScroll() {
+    if (menuBtn.classList.contains('side-menu__isShowing')) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = 'auto';
+    }
 }
