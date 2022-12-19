c008d6dd073d2649e69e2f05ff4dd18919b2508a
valvanogabryel <valvanogabryel@gmail.com>
2022-12-19 11:22:22 -0300

    /

---
+++ javascript-na-web__armazenando-dados-da-web/mochila-de-viagem/script/main.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('#novoItem');
+const list = document.querySelector('ul.lista');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const elementName = event.target.elements['nome'].value;
+    const elementQuantity = event.target.elements['quantidade'].value;
+
+    registerItem(elementName, elementQuantity);
+})
+
+function registerItem(name, quantity) {
+    const listElement = document.createElement('li');
+    listElement.className = 'item';
+    listElement.innerHTML = `<strong>${quantity}</strong>${name}`
+
+    list.appendChild(listElement);
+}
