e7fdc0572f744ed08aa01f5077e02ab0f672402e
valvanogabryel <valvanogabryel@gmail.com>
2022-12-19 14:12:06 -0300

    Update main.js

--- javascript-na-web__armazenando-dados-da-web/mochila-de-viagem/script/main.js
+++ javascript-na-web__armazenando-dados-da-web/mochila-de-viagem/script/main.js
@@ -3,7 +3,7 @@ const list = document.querySelector('ul.lista');
 const items = JSON.parse(localStorage.getItem('items')) || [];
 
 items.forEach((element) => {
-    registerItem(element.name, element.quantity)
+    registerItem(element.name, element.quantity);
 });
 
 form.addEventListener('submit', (event) => {
@@ -11,8 +11,20 @@ form.addEventListener('submit', (event) => {
     const elementName = event.target.elements['nome'];
     const elementQuantity = event.target.elements['quantidade'];
 
+    const isInList = items.find(element => {
+        return element.name === elementName.value;
+    })
+
+
+    if (isInList) {
+
+    } else {
+
+    }
+
     registerItem(elementName.value, elementQuantity.value);
     storageItem(elementName.value, elementQuantity.value);
+
 
     // clean form value
     elementName.value = '';
@@ -23,6 +35,8 @@ function registerItem(name, quantity) {
     const listElement = document.createElement('li');
     listElement.className = 'item';
     listElement.innerHTML = `<strong>${quantity}</strong>${name}`;
+    listElement.dataset.id = 0;
+    console.log(listElement)
     list.appendChild(listElement);
 }
 
@@ -34,8 +48,14 @@ function storageItem(name, quantity) {
 
     items.push(currentItem);
 
+
     localStorage.setItem('items', JSON.stringify(items));
+
 }
+
+
+
+
 
 
 
