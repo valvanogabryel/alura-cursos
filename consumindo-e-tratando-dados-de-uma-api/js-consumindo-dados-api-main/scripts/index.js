e5ff72155f7fb56827d894b1eff2446e1e1c5e30
valvanogabryel <valvanogabryel@gmail.com>
2022-12-23 11:09:11 -0300

    Update index.js

--- consumindo-e-tratando-dados-de-uma-api/js-consumindo-dados-api-main/scripts/index.js
+++ consumindo-e-tratando-dados-de-uma-api/js-consumindo-dados-api-main/scripts/index.js
@@ -1,24 +1,31 @@
-async function searchCEP() {
-    let response = await fetch('https://viacep.com.br/ws/21073430/json/');
-    let cep = await response.json();
-    return cep;
+async function searchCEP(cepNumber) {
+    try {
+        let response = await fetch(`https://viacep.com.br/ws/${cepNumber}/json/`);
+        let cep = await response.json();
+        if (cep.erro) {
+            throw new Error('CEP inexistente...');
+        };
+        fillCep(cep)
+        return cep;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
+const cepInput = document.querySelector('input#cep');
 
+cepInput.addEventListener('focusout', () => {
+    searchCEP(cepInput.value)
+})
 
+function fillCep(cep) {
+    const city = document.getElementById('cidade');
+    const publicPlace = document.getElementById('endereco');
+    const neighbourhood = document.getElementById('bairro');
+    const state = document.getElementById('estado');
 
-//     .then(response => response.json())
-//     .then(cepObject => {
-//         if (cepObject.erro) {
-//             throw new Error('Esse Cep não existe');
-//         } else console.log(cepObject);
-//     })
-//     .catch(error => console.log(error))
-//     .finally(message => console.log('Processamento concluído'));
-
-searchCEP()
-console.log(cep);
-
-
-
-
+    neighbourhood.value = cep.bairro;
+    city.value = cep.localidade;
+    publicPlace.value = cep.logradouro;
+    state.value = cep.uf;
+}
