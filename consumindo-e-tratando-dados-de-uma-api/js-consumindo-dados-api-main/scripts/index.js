0cc3f74773a24a191a7d7710eac22f0b3702c841
valvanogabryel <valvanogabryel@gmail.com>
2022-12-23 10:18:27 -0300

    Update index.js

---
+++ consumindo-e-tratando-dados-de-uma-api/js-consumindo-dados-api-main/scripts/index.js
@@ -0,0 +1,20 @@
+async function searchCEP() {
+    let response = await fetch('https://viacep.com.br/ws/21073430/json/');
+    let cep = response.json();
+    return cep;
+}
+
+
+
+
+//     .then(response => response.json())
+//     .then(cepObject => {
+//         if (cepObject.erro) {
+//             throw new Error('Esse Cep não existe');
+//         } else console.log(cepObject);
+//     })
+//     .catch(error => console.log(error))
+//     .finally(message => console.log('Processamento concluído'));
+
+console.log(cep);
+
