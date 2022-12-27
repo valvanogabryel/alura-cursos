8ec18a9a938332d0983c1caf6c3996a9d3b28c9c
valvanogabryel <valvanogabryel@gmail.com>
2022-12-27 15:43:37 -0300

    ;

--- criando-requisições/aluraplay-requisicoes-main/js/connectApi.js
+++ criando-requisições/aluraplay-requisicoes-main/js/connectApi.js
@@ -1,7 +1,6 @@
 async function getVideos() {
     const response = await fetch('http://localhost:3000/videos');
     const videos = await response.json();
-    console.table(videos)
     return videos;
 }
 
--- criando-requisições/aluraplay-requisicoes-main/js/showVideos.js
+++ criando-requisições/aluraplay-requisicoes-main/js/showVideos.js
@@ -1,1 +1,27 @@
+import { connectApi } from "./connectApi.js";
+
 const list = document.querySelector('[data-lista]');
+
+function createListElement(listElement) {
+    const video = document.createElement('li');
+    video.className = 'videos__item';
+    video.innerHTML =
+        `<iframe width="100%" height="72%" src="${listElement.url}"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>
+<div class="descricao-video">
+    <img src="${listElement.imagem}">
+    <h3>${listElement.titulo}</h3>
+    <p>${listElement.descricao}</p>
+</div>`
+
+    return video;
+}
+
+async function videoList() {
+    const apiList = await connectApi.getVideos();
+    apiList.forEach(element => list.appendChild(createListElement(element)));
+}
+
+videoList();
