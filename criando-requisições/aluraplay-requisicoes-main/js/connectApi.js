44016b0166e20d0bcd19bd6a124b08c6d6eb4a00
valvanogabryel <valvanogabryel@gmail.com>
2022-12-28 09:34:11 -0300

    Update connectApi.js

--- criando-requisições/aluraplay-requisicoes-main/js/connectApi.js
+++ criando-requisições/aluraplay-requisicoes-main/js/connectApi.js
@@ -4,6 +4,25 @@ async function getVideos() {
     return videos;
 }
 
+async function createVideos(element) {
+    const response = await fetch('http://localhost:3000/videos', {
+        method: 'POST',
+        headers: {
+            'Content-type': "application/json"
+        },
+        body: JSON.stringify({
+            titulo: element.titulo,
+            descricao: `${element.descricao} mil vizualizações`,
+            url: element.url,
+            imagem: element.imagem
+        })
+    });
+
+    const convertedResponse = await response.json();
+    return convertedResponse;
+}
+
 export const connectApi = {
-    getVideos
+    getVideos,
+    createVideos
 }
