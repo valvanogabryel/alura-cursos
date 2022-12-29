937f03840e509f5fbf0ed9a40d6e44b6ff43b55a
valvanogabryel <valvanogabryel@gmail.com>
2022-12-29 11:22:47 -0300

    Update camera.js

--- validando-formularios/monibank-main/pages/js/camera.js
+++ validando-formularios/monibank-main/pages/js/camera.js
@@ -4,6 +4,8 @@ const cameraField = document.querySelector('[data-camera]');
 const takePicBtn = document.querySelector('[data-tirar-foto]');
 const canvas = document.querySelector('[data-video-canvas]');
 const message = document.querySelector('[data-mensagem]');
+const sendPicBtn = document.querySelector('[data-enviar]');
+let imageURL = '';
 
 startCameraBtn.addEventListener('click', async () => {
     const startVideo = await navigator.mediaDevices.getUserMedia({
@@ -26,39 +28,13 @@ takePicBtn.addEventListener('click', function () {
     message.style.display = 'block';
 })
 
+sendPicBtn.addEventListener('click', () => {
+    const receiveData = localStorage.getItem("cadastro");
+    const convertData = JSON.parse(receiveData);
 
+    convertData.imagem = imageURL;
 
+    localStorage.setItem('cadastro', JSON.stringify(convertData))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// window.location.href = '../pages/abrir-conta-form-3.html';
+    window.location.href = '../pages/abrir-conta-form-3.html';
+})
