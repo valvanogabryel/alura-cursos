a06b7f09708d90fc45d2f9cb6b3b51ff33bb28a3
valvanogabryel <valvanogabryel@gmail.com>
2022-12-29 11:14:01 -0300

    Update camera.js

--- validando-formularios/monibank-main/pages/js/camera.js
+++ validando-formularios/monibank-main/pages/js/camera.js
@@ -3,6 +3,7 @@ const video = document.querySelector('[data-video]');
 const cameraField = document.querySelector('[data-camera]');
 const takePicBtn = document.querySelector('[data-tirar-foto]');
 const canvas = document.querySelector('[data-video-canvas]');
+const message = document.querySelector('[data-mensagem]');
 
 startCameraBtn.addEventListener('click', async () => {
     const startVideo = await navigator.mediaDevices.getUserMedia({
@@ -14,6 +15,15 @@ startCameraBtn.addEventListener('click', async () => {
     cameraField.style.display = 'block';
 
     video.srcObject = startVideo;
+})
+
+takePicBtn.addEventListener('click', function () {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    imageURL = canvas.toDataURL('image/jpeg');
+
+    cameraField.style.display = 'none';
+    message.style.display = 'block';
 })
 
 
