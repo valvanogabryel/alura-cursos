b6759575a903876e5b919386423916c9cfc281cc
valvanogabryel <valvanogabryel@gmail.com>
2023-01-02 09:00:25 -0300

    Update negotiation.ts

--- formacao-typescript/parte1/typescript-curso-1-arquivos-iniciais/app/models/negotiation.ts
+++ formacao-typescript/parte1/typescript-curso-1-arquivos-iniciais/app/models/negotiation.ts
@@ -1,22 +1,11 @@
 export class Negotiation {
-    constructor(private _date: Date,
-        private _quantity: number,
-        private _value: number
+    constructor(
+        private readonly date: Date,
+        private readonly quantity: number,
+        private readonly value: number
     ) { }
 
-    get date(): Date {
-        return this._date;
-    }
-
-    get quantity(): number {
-        return this._quantity;
-    }
-
-    get value(): number {
-        return this._value;
-    }
-
     get volume(): number {
-        return this._quantity * this._value;
+        return this.quantity * this.value;
     }
 }
