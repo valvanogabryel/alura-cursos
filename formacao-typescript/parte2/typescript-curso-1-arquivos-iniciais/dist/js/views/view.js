fcee57af8f906a85eda565f988c52143e422cbd5
valvanogabryel <valvanogabryel@gmail.com>
2023-01-04 10:56:06 -0300

    /

--- formacao-typescript/parte2/typescript-curso-1-arquivos-iniciais/dist/js/views/view.js
+++ formacao-typescript/parte2/typescript-curso-1-arquivos-iniciais/dist/js/views/view.js
@@ -1,7 +1,12 @@
 export class View {
+    //                            o "?" faz com que o parâmetro seja opicional. 
     constructor(selector, scape) {
         this.scape = false;
         this.element = document.querySelector(selector);
+        if (scape) {
+            this.scape = scape;
+        }
+        ; //Caso nenhum parâmetro seja passado, o valor de "scape" será undefined. Ou seja, false, e não entrará no bloco de código. Caso seja true, o valor booleano será atribuído ao scape.
     }
     update(model) {
         let template = this.template(model);
