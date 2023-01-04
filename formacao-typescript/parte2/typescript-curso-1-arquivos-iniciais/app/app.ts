221710988aeb2c4e6a3dd3558e46546ee2fb6697
valvanogabryel <valvanogabryel@gmail.com>
2023-01-04 11:31:27 -0300

    ;

--- formacao-typescript/parte2/typescript-curso-1-arquivos-iniciais/app/app.ts
+++ formacao-typescript/parte2/typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -3,8 +3,12 @@ import { NegotiationController } from "./controllers/negotiation-controller.js";
 const controller = new NegotiationController();
 const form = document.querySelector('[data-form]');
 
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    controller.add();
-});
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.add();
+    });
+} else {
+    throw new Error('Não foi possível iniciar a aplicação');
+}
 
