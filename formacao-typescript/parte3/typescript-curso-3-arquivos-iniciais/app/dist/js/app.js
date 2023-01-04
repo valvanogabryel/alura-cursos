cc14e7bff5515308fe8f121f2a719896fc2745e8
valvanogabryel <valvanogabryel@gmail.com>
2023-01-04 12:18:06 -0300

    ,.

--- formacao-typescript/parte3/typescript-curso-3-arquivos-iniciais/app/dist/js/app.js
+++ formacao-typescript/parte3/typescript-curso-3-arquivos-iniciais/app/dist/js/app.js
@@ -1,5 +1,5 @@
-import { NegotiationController } from "./controllers/negotiation-controller.js";
-const controller = new NegotiationController();
+import { NegotiationController } from "../app/src/controllers/negotiation-controller.js";
+const controller = new NegotiationController;
 const form = document.querySelector('[data-form]');
 if (form) {
     form.addEventListener('submit', event => {
--- formacao-typescript/parte3/typescript-curso-3-arquivos-iniciais/app/src/views/view.ts
+++ formacao-typescript/parte3/typescript-curso-3-arquivos-iniciais/app/src/views/view.ts
@@ -1,3 +1,5 @@
+import { logExecutionTime } from "../decorators/log-execution-time.js";
+
 export abstract class View<T> {
     //abstract impede a instância da classe View.
     protected element: HTMLElement;
@@ -16,6 +18,7 @@ export abstract class View<T> {
         if (scape) { this.scape = scape }; //Caso nenhum parâmetro seja passado, o valor de "scape" será undefined. Ou seja, false, e não entrará no bloco de código. Caso seja true, o valor booleano será atribuído ao scape.
     }
 
+    @logExecutionTime()
     update(model: T): void {
         let template = this.template(model);
         // removes script tags
