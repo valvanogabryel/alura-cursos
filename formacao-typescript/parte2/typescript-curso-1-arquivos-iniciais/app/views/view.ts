319a5c873f18a3da235c6e9f021c9107cd8e0775
valvanogabryel <valvanogabryel@gmail.com>
2023-01-04 10:43:46 -0300

    Update view.ts

--- formacao-typescript/parte2/typescript-curso-1-arquivos-iniciais/app/views/view.ts
+++ formacao-typescript/parte2/typescript-curso-1-arquivos-iniciais/app/views/view.ts
@@ -1,13 +1,15 @@
 export abstract class View<T> {
     //abstract impede a instância da classe View.
     protected element: HTMLElement;
+    private scape = false;
 
     constructor(selector: string) {
         this.element = document.querySelector(selector);
     }
 
     update(model: T): void {
-        const template = this.template(model);
+        let template = this.template(model);
+        if (this.scape) { template = template.replace(/<script>[\s\S]*?<\/script>/, '') };
         this.element.innerHTML = template;
     }
 
