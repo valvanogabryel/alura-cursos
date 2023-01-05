69a46b0eb903d1bc053de9352df099b0828a224a
valvanogabryel <valvanogabryel@gmail.com>
2023-01-05 11:56:13 -0300

    Update inspect.ts

--- formacao-typescript/parte3/typescript-curso-3-arquivos-iniciais/app/src/decorators/inspect.ts
+++ formacao-typescript/parte3/typescript-curso-3-arquivos-iniciais/app/src/decorators/inspect.ts
@@ -1,19 +1,17 @@
-export function inspect() {
-    return function (
-        target: any,
-        propertyKey: string,
-        descriptor: PropertyDescriptor
-    ) {
-        const originalMethod = descriptor.value;
-        descriptor.value = function (...args: any[]) {
-            console.log(`--- Método: ${propertyKey}`);
-            console.log(`------ Parâmetros: ${JSON.stringify(args)}`);
-            const returnedMethod = originalMethod.apply(this, args);
-            console.log(`------ Retorno: ${returnedMethod}`);
+export function inspect(
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+        console.log(`--- Método: ${propertyKey}`);
+        console.log(`------ Parâmetros: ${JSON.stringify(args)}`);
+        const returnedMethod = originalMethod.apply(this, args);
+        console.log(`------ Retorno: ${returnedMethod}`);
 
-            return returnedMethod;
-        }
-
-        return descriptor;
+        return returnedMethod;
     }
+
+    return descriptor;
 }
