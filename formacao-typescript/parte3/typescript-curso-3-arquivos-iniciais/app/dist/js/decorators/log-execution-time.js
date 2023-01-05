150fb0903d86a5cd5e4b829eb8597128c90defa5
valvanogabryel <valvanogabryel@gmail.com>
2023-01-05 10:48:37 -0300

    ;

--- formacao-typescript/parte3/typescript-curso-3-arquivos-iniciais/app/dist/js/decorators/log-execution-time.js
+++ formacao-typescript/parte3/typescript-curso-3-arquivos-iniciais/app/dist/js/decorators/log-execution-time.js
@@ -4,6 +4,8 @@ export function logExecutionTime() {
         descriptor.value = function (...args) {
             const t1 = performance.now();
             const returnedMethod = originalMethod.apply(this, args);
+            console.log(this);
+            console.log(args);
             const t2 = performance.now();
             console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / 1000}`);
             returnedMethod;
