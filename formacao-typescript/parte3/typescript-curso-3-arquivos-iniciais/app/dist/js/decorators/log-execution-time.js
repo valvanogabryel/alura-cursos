8fc12ea3b14227585bfbffdde6b8757d73ff07ad
valvanogabryel <valvanogabryel@gmail.com>
2023-01-05 11:16:34 -0300

    /

--- formacao-typescript/parte3/typescript-curso-3-arquivos-iniciais/app/dist/js/decorators/log-execution-time.js
+++ formacao-typescript/parte3/typescript-curso-3-arquivos-iniciais/app/dist/js/decorators/log-execution-time.js
@@ -6,7 +6,7 @@ export function logExecutionTime() {
             const returnedMethod = originalMethod.apply(this, args);
             const t2 = performance.now();
             console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / 1000}`);
-            returnedMethod;
+            return returnedMethod;
         };
         return descriptor;
     };
