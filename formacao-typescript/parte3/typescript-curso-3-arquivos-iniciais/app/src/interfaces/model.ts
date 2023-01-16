b9aa2a71c4d8e88bf0e58e57e4258fd61a6ab1bc
valvanogabryel <valvanogabryel@gmail.com>
2023-01-16 10:06:44 -0300

    Update model.ts

--- formacao-typescript/parte3/typescript-curso-3-arquivos-iniciais/app/src/interfaces/model.ts
+++ formacao-typescript/parte3/typescript-curso-3-arquivos-iniciais/app/src/interfaces/model.ts
@@ -3,5 +3,4 @@ import { Comparable } from "./comparable.js";
 //   Classes não podem "extender" mais de uma interface, somente "implementar". Porém, interfaces podem "extender" outras interfaces.
 export interface Model<T> extends Printable, Comparable<T> {
     //a interface Model obrigará as classes cujo implementarem-na a ter os métodos "toText()" e "isEqual()"
-
 }   
