a2c9b9d558e0138f9bf90711499957d8f637178f
valvanogabryel <valvanogabryel@gmail.com>
2023-01-17 11:44:54 -0300

    ;

---
+++ formações/formacao_react_com_javascript/organograma/src/App.js
@@ -0,0 +1,13 @@
+import Banner from './components/Banner/Banner.js';
+import TextField from './components/TextField/TextField.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <TextField />
+    </div>
+  );
+}
+
+export default App;
---
+++ formações/formacao_react_com_javascript/organograma/src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './banner.css';
+
+const Banner = () => {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal da Organo"></img>
+        </header>
+    )
+}
+
+export default Banner;
