5b91a241f9a8a12ba8db437c05929538a3cc0cca
valvanogabryel <valvanogabryel@gmail.com>
2023-01-19 12:48:31 -0300

    /

---
+++ formações/formacao_react_com_javascript/organograma/src/components/Collaborator/index.js
@@ -0,0 +1,17 @@
+import './Collaborator.css';
+
+const Collaborator = (props) => {
+    return (
+        <div>
+            <div>
+                <img src="#" alt="Foto de perfil do colaborador" />
+            </div>
+            <div>
+                <h4>Gabryel Valvano</h4>
+                <h5>Desenvolvedor Front-End</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Collaborator;
