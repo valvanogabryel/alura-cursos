9e5f3613b08f06200d7c02a51b1a5d3b26facb45
valvanogabryel <valvanogabryel@gmail.com>
2023-01-20 10:11:23 -0300

    Revert "Update index.js"
    This reverts commit 43f88f597708140640d2055832e0c4e37885594e.

--- formações/formacao_react_com_javascript/organograma/src/components/Collaborator/index.js
+++ formações/formacao_react_com_javascript/organograma/src/components/Collaborator/index.js
@@ -10,7 +10,7 @@ const Collaborator = ({ name, role, image, primary_color, onDelete }) => {
                 <img src={image} alt="Foto de perfil do colaborador" />
             </div>
             <div className="card__informations">
-                <h4>{name.toUpperCase()}</h4>
+                <h4>{name}</h4>
                 <h5>{role}</h5>
             </div>
         </div>
