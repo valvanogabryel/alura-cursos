227a533bf26101d904ec5e27f627e2e4bca950eb
valvanogabryel <valvanogabryel@gmail.com>
2023-01-20 08:54:22 -0300

    ;

--- formações/formacao_react_com_javascript/organograma/src/components/Team/index.js
+++ formações/formacao_react_com_javascript/organograma/src/components/Team/index.js
@@ -1,29 +1,37 @@
+
+import { Fragment } from 'react';
 import Collaborator from '../Collaborator';
 import './Team.css';
 
 const Team = (props) => {
     const sectionBackground = { backgroundColor: props.secondary_color }
 
+
     return (
         // RENDERIZAÇÃO CONDICIONAL EM REACT
         //  (props.collaborators.length > 0) && 
         //              OU
         (props.collaborators.length > 0) ?
-            <section className='team__container' style={sectionBackground}>
-                <h3 className='team__title' style={{ borderColor: props.primary_color }}>{props.name}</h3>
-                <div className="team__collaborators">
-                    {props.collaborators.map((collaborator, index) => {
-                        return <Collaborator
-                            name={collaborator.name}
-                            role={collaborator.role}
-                            image={collaborator.image}
-                            primary_color={props.primary_color}
-                            key={index}
-                        />
-                    })}
-                </div>
-            </section>
+            <Fragment>
+                <h1 className='organization__title'>Minha Organização:</h1>
+                <span className='organization__line'>-</span>
+                <section className='team__container' style={sectionBackground}>
+                    <h3 className='team__title' style={{ borderColor: props.primary_color }}>{props.name}</h3>
+                    <div className="team__collaborators">
+                        {props.collaborators.map((collaborator, index) => {
+                            return <Collaborator
+                                name={collaborator.name}
+                                role={collaborator.role}
+                                image={collaborator.image}
+                                primary_color={props.primary_color}
+                                key={index}
+                            />
+                        })}
+                    </div>
+                </section>
+            </Fragment>
             : ''
+
     )
 }
 
