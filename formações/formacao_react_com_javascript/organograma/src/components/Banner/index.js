5f02d071bff7818f98049b8928d4f2393d95efae
valvanogabryel <valvanogabryel@gmail.com>
2023-01-23 10:24:18 -0300

    /

--- formações/formacao_react_com_javascript/organograma/src/components/Banner/index.js
+++
@@ -1,9 +0,0 @@
-import './banner.css';
-
-const Banner = (props) => (
-    <header className="banner__container">
-        <img className="banner__image" src={props.src} alt={props.alt}></img>
-    </header>
-)
-
-export default Banner;
