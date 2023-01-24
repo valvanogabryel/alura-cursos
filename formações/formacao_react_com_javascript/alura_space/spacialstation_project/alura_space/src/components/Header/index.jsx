06f83f04f472ab10b4396c0027b6c0f367e284a3
valvanogabryel <valvanogabryel@gmail.com>
2023-01-24 09:02:15 -0300

    ;

--- formações/formacao_react_com_javascript/alura_space/spacialstation_project/alura_space/src/components/Header/index.jsx
+++ formações/formacao_react_com_javascript/alura_space/spacialstation_project/alura_space/src/components/Header/index.jsx
@@ -1,14 +1,14 @@
+import logo from './images/logo.png';
+import search from './images/search.png';
 
 const Header = () => {
     return (
         <header>
-            <img src="/assets/logo.png" alt="Logo da Alura Space" />
+            <img src={logo} alt="Logo da Alura Space" />
             <div>
-                <input type="search" />
-                <img src="/assets/search.png" alt="" />
+                <input type="search" placeholder="O que você procura?" />
+                <img src={search} alt="Ícone de pesquisa" />
             </div>
-
-
         </header>
     )
 }
