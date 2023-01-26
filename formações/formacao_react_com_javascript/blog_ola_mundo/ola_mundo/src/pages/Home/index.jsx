3ca68fe19284f14358941f3021185ae4ccc53139
valvanogabryel <valvanogabryel@gmail.com>
2023-01-26 09:59:31 -0300

    /

--- formações/formacao_react_com_javascript/blog_ola_mundo/ola_mundo/src/pages/Home/index.jsx
+++ formações/formacao_react_com_javascript/blog_ola_mundo/ola_mundo/src/pages/Home/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Banner from 'components/Banner';
 
 const Home = () => {
-    return (<h1>Hello, World!</h1>);
+    return (
+        <main>
+            <Banner />
+            <h1>Hello, World!</h1>
+        </main>
+    )
 }
 
 export default Home;
