323728fda4d8556605b7da06afd666a62ca98614
valvanogabryel <valvanogabryel@gmail.com>
2023-01-26 11:16:57 -0300

    Update Routes.js

--- formações/formacao_react_com_javascript/blog_ola_mundo/ola_mundo/src/Routes.js
+++ formações/formacao_react_com_javascript/blog_ola_mundo/ola_mundo/src/Routes.js
@@ -13,11 +13,28 @@ export default function AppRoutes() {
         {/* Rotas aninhadas: */}
         {/* As rotas <Home/> e <AboutMe/> tornaram-se rotas filhas de <DefaultPage/> */}
         {/* Indica-se, na rota pai, onde vai ser colocada alguma das rotas que forem renderizadas. */}
-        {/* A rota pai funcionará como um "roteador" também, escolhendo uma das rotas filhas a serem renderizadas. */}
+        {/* A rota pai funcionará como um "roteador" também, escolhendo uma das rotas filhas a serem renderizadas de acordo com o caminho da url. */}
         <Route path="/" element={<DefaultPage />}>
           <Route path="/" element={<Home />} />
           <Route path="/aboutme" element={<AboutMe />} />
         </Route>
+        {
+          /* 
+          Na Rota "/", a estrutura a ser renderizada é:
+          {
+            <DefaultPage>
+            <Home/>
+            </DefaultPage>
+          }
+  
+           Na Rota "/aboutme", a estrutura a ser renderizada é:
+          {
+            <DefaultPage>
+            <AboutMe/>
+            </DefaultPage>
+          }
+           */
+        }
 
         {/* <Route path="*" element={<Page404 />} /> */}
       </Routes>
