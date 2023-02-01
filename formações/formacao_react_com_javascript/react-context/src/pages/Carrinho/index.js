a2e7fabdbd73388b0885f7185dabb8f1d12b1a6f
valvanogabryel <valvanogabryel@gmail.com>
2023-02-01 13:45:54 -0300

    ;

--- formações/formacao_react_com_javascript/react-context/src/pages/Carrinho/index.js
+++ formações/formacao_react_com_javascript/react-context/src/pages/Carrinho/index.js
@@ -1,33 +1,49 @@
 import { Button, Snackbar, InputLabel } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
+import { useCarrinhoContext } from 'common/context/Carrinho';
+import Produto from 'components/Produto';
 import { useState } from 'react';
-import { Container, Voltar, TotalContainer, PagamentoContainer} from './styles';
+import { useHistory } from 'react-router-dom';
+import { Container, Voltar, TotalContainer, PagamentoContainer } from './styles';
 
 function Carrinho() {
   const [openSnackbar, setOpenSnackbar] = useState(false);
+  const { carrinho } = useCarrinhoContext();
+  const history = useHistory();
+
   return (
     <Container>
-      <Voltar />
+      <Voltar
+        onClick={() => history.push('/feira')}
+      />
       <h2>
         Carrinho
       </h2>
+      {
+        carrinho.map(product => (
+          <Produto
+            {...product}
+            key={product.id}
+          />
+        ))
+      }
       <PagamentoContainer>
         <InputLabel> Forma de Pagamento </InputLabel>
       </PagamentoContainer>
       <TotalContainer>
-          <div>
-            <h2>Total no Carrinho: </h2>
-            <span>R$ </span>
-          </div>
-          <div>
-            <h2> Saldo: </h2>
-            <span> R$ </span>
-          </div>
-          <div>
-            <h2> Saldo Total: </h2>
-            <span> R$ </span>
-          </div>
-        </TotalContainer>
+        <div>
+          <h2>Total no Carrinho: </h2>
+          <span>R$ </span>
+        </div>
+        <div>
+          <h2> Saldo: </h2>
+          <span> R$ </span>
+        </div>
+        <div>
+          <h2> Saldo Total: </h2>
+          <span> R$ </span>
+        </div>
+      </TotalContainer>
       <Button
         onClick={() => {
           setOpenSnackbar(true);
@@ -35,25 +51,25 @@ function Carrinho() {
         color="primary"
         variant="contained"
       >
-         Comprar
-       </Button>
-        <Snackbar
-          anchorOrigin={
-            { 
-              vertical: 'top',
-              horizontal: 'right'
-            }
+        Comprar
+      </Button>
+      <Snackbar
+        anchorOrigin={
+          {
+            vertical: 'top',
+            horizontal: 'right'
           }
-          open={openSnackbar}
+        }
+        open={openSnackbar}
+        onClose={() => setOpenSnackbar(false)}
+      >
+        <MuiAlert
           onClose={() => setOpenSnackbar(false)}
+          severity="success"
         >
-           <MuiAlert
-            onClose={() => setOpenSnackbar(false)}
-            severity="success"
-          >
-            Compra feita com sucesso!
-          </MuiAlert>
-        </Snackbar>
+          Compra feita com sucesso!
+        </MuiAlert>
+      </Snackbar>
     </Container>
   )
 }
