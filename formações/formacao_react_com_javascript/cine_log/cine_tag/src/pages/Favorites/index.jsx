f18d85fe60a8d9d10a04c19959976acb8b77dce1
valvanogabryel <valvanogabryel@gmail.com>
2023-02-06 12:19:48 -0300

    ;

--- formações/formacao_react_com_javascript/cine_log/cine_tag/src/pages/Favorites/index.jsx
+++ formações/formacao_react_com_javascript/cine_log/cine_tag/src/pages/Favorites/index.jsx
@@ -8,25 +8,41 @@ import CardContainer from 'components/CardContainer';
 import favoritesBanner from 'assets/banner-favoritos.png';
 import { useFavoriteContext } from 'common/context/Favorites';
 
+import styles from './Favorites.module.css';
+
 const Favorites = () => {
     const { favorites } = useFavoriteContext();
+
+    if (favorites.length === 0) {
+        return (
+            <>
+                <Banner banner={favoritesBanner} />
+                <section className={styles.container}>
+                    <p>Você não tem <span>nenhum</span> filme favoritado :(</p>
+                </section>
+            </>
+        )
+    }
+
     return (
         <>
             <Banner banner={favoritesBanner} />
-            <Title>
-                <h1>Meus Favoritos</h1>
-            </Title>
-            <CardContainer>
-                {
-                    favorites.map(fav =>
-                        <Card
-                            key={fav.id}
-                            id={fav.id}
-                            title={fav.title}
-                            cover={fav.cover}
-                        />)
-                }
-            </CardContainer>
+            <section className={styles.container}>
+                <Title>
+                    <h1>Meus Favoritos</h1>
+                </Title>
+                <CardContainer>
+                    {
+                        favorites.map(fav =>
+                            <Card
+                                key={fav.id}
+                                id={fav.id}
+                                title={fav.title}
+                                cover={fav.cover}
+                            />)
+                    }
+                </CardContainer>
+            </section>
         </>
     );
 }
