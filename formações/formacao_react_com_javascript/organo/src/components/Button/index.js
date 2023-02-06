daad441e73d6aa3bf1225ee61d99124c4f485c0c
valvanogabryel <valvanogabryel@gmail.com>
2023-02-06 13:23:20 -0300

    ts_migration

--- formações/formacao_react_com_javascript/organo/src/components/Button/index.js
+++
@@ -1,9 +0,0 @@
-import './Button.css'
-
-const Button = (props) => {
-    return (<button className='botao'>
-        {props.children}
-    </button>)
-}
-
-export default Button;
---
+++ formações/formacao_react_com_javascript/organo/src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ReactElement } from 'react';
+import './Button.css';
+
+interface ButtonProps {
+    children: ReactElement | string
+}
+
+const Button = (props: ButtonProps) => {
+    return (
+        <button className='botao'>
+            {props.children}
+        </button>)
+}
+
+export default Button;
