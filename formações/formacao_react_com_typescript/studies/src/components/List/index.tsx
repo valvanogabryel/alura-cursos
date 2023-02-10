fdcd05ebc06ab300913a92b4538c8c2d61d772ed
valvanogabryel <valvanogabryel@gmail.com>
2023-02-10 08:51:55 -0300

    Update index.tsx

--- formações/formacao_react_com_typescript/studies/src/components/List/index.tsx
+++ formações/formacao_react_com_typescript/studies/src/components/List/index.tsx
@@ -1,25 +1,29 @@
 import ListItem from "./ListItem";
 import styles from './List.module.scss';
+import { useState } from "react";
 
 const List = () => {
-    const tasks = [
-        {
-            taskName: 'React',
-            studyTime: '01:30:00'
-        },
-        {
-            taskName: 'TypeScript',
-            studyTime: '02:12:00'
-        },
-        {
-            taskName: 'SASS',
-            studyTime: '00:30:00'
-        },
-    ];
+    const [tasks, setTasks] = useState([{
+        taskName: 'React',
+        studyTime: '01:30:00'
+    },
+    {
+        taskName: 'TypeScript',
+        studyTime: '02:12:00'
+    },
+    {
+        taskName: 'SASS',
+        studyTime: '00:30:00'
+    },
+    ])
+
+
 
     return (
         <section className={styles.taskList}>
-            <h2>Estudos do dia</h2>
+            <h2 onClick={() => {
+                setTasks([...tasks, { taskName: 'Bootstrap', studyTime: '00:45:00' }])
+            }}>Estudos do dia</h2>
             <ul>
                 {
                     tasks.map((task, index) =>
