807ecae3bcb4cd6ce02219d4e8e2830842aafdce
valvanogabryel <valvanogabryel@gmail.com>
2023-02-17 13:03:57 -0300

    ;

--- formações/formacao_react_com_typescript/aifood/src/componentes/Banner/index.tsx
+++ formações/formacao_react_com_typescript/aifood/src/componentes/Banner/index.tsx
@@ -3,7 +3,7 @@ import estilos from './Banner.module.scss';
 const Banner = () => {
   return (<section className={estilos.BannerArea}>
     <div className={estilos.Container}>
-      <h1 className={estilos.Titulo}>AlFood</h1>
+      <h1 className={estilos.Titulo}>AiFood</h1>
       <p>Felicidade em cada prato.</p>
     </div>
   </section>)
--- formações/formacao_react_com_typescript/aifood/src/componentes/Rodape/index.tsx
+++ formações/formacao_react_com_typescript/aifood/src/componentes/Rodape/index.tsx
@@ -3,7 +3,7 @@ import estilos from './Rodape.module.scss';
 const NavBar = () => {
   return (<footer className={estilos.Rodape}>
     <div>
-      <p>Copyright &copy; {new Date().getFullYear()} Alfood</p>
+      <p>Copyright &copy; {new Date().getFullYear()} Aifood</p>
     </div>
     <div>
       <ul className="social-icons">
