2a86faddb442b9e6a470c682b949d898b1d09523
valvanogabryel <valvanogabryel@gmail.com>
2023-02-17 11:33:19 -0300

    ;

--- formações/formacao_react_com_typescript/aluroni/src/components/Admin/index.tsx
+++ formações/formacao_react_com_typescript/aluroni/src/components/Admin/index.tsx
@@ -1,17 +1,35 @@
-import { useParams } from 'react-router-dom';
-
+// react
+import { useEffect } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+// styles
 import styles from './Admin.module.scss';
+import themeStyles from 'styles/Theme.module.scss';
+// libraries
+import classNames from 'classnames';
 
 const Admin = () => {
     const { user } = useParams();
 
+    useEffect(() => {
+        window.scrollTo(
+            100,
+            100
+        );
+    }, [window]);
+
 
     return (
-        user === 'gabryel' ?
-            <div>
-                {`Olá, ${user}`}
-            </div>
-            : ''
+        (user === 'gabryel') ?
+            <section className={classNames(
+                themeStyles.container,
+                styles.admin
+            )}>
+                <div className={styles.admin__greetings}>
+                    <h3 className={themeStyles.title}>Olá, {user}</h3>
+                </div>
+            </section>
+            :
+            <Navigate to='/' />
     );
 };
 
