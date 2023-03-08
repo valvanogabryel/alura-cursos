d1b793037ed5d9b6da61cb0a1f71fefd0f9a7278
valvanogabryel <valvanogabryel@gmail.com>
2023-03-08 10:58:59 -0300

    ;]

--- formações/formacao_react_com_typescript/event-tracker/src/components/Formulario/index.tsx
+++ formações/formacao_react_com_typescript/event-tracker/src/components/Formulario/index.tsx
@@ -3,13 +3,13 @@ import { IEvento } from '../../interfaces/IEvento';
 import style from './Formulario.module.scss';
 
 const Formulario: React.FC<{ aoSalvar: (evento: IEvento) => void }> = ({ aoSalvar }) => {
-  const [descricao, setDescricao] = useState('')
-  const [dataInicio, setDataInicio] = useState('')
-  const [horaInicio, setHoraInicio] = useState('')
-  const [dataFim, setDataFim] = useState('')
-  const [horaFim, setHoraFim] = useState('')
+  const [descricao, setDescricao] = useState('');
+  const [dataInicio, setDataInicio] = useState('');
+  const [horaInicio, setHoraInicio] = useState('');
+  const [dataFim, setDataFim] = useState('');
+  const [horaFim, setHoraFim] = useState('');
 
-  const montarData = (data:string, hora: string) => {
+  const montarData = (data: string, hora: string) => {
     const dataString = data.slice(0, 10)
     return new Date(`${dataString}T${hora}`)
   }
@@ -32,51 +32,51 @@ const Formulario: React.FC<{ aoSalvar: (evento: IEvento) => void }> = ({ aoSalva
     <h3 className={style.titulo}>Novo evento</h3>
 
     <label>Descrição</label>
-    <input 
-      type="text" 
+    <input
+      type="text"
       name="descricao"
       id="descricao"
       className={style.input}
-      onChange={evento => setDescricao(evento.target.value)} 
-      placeholder="Descrição" value={descricao} 
+      onChange={evento => setDescricao(evento.target.value)}
+      placeholder="Descrição" value={descricao}
       autoComplete="off"
       required />
 
-      <label>Data de início</label>
-      <div className={style.inputContainer}>
-        <input 
-          type="date" 
-          name="dataInicio"
-          className={style.input}
-          onChange={evento => setDataInicio(evento.target.value)} 
-          value={dataInicio}
-          required />
-        <input 
-          type="time" 
-          name="horaInicio"
-          className={style.input}
-          onChange={evento => setHoraInicio(evento.target.value)} 
-          value={horaInicio}
-          required />
-      </div>
+    <label>Data de início</label>
+    <div className={style.inputContainer}>
+      <input
+        type="date"
+        name="dataInicio"
+        className={style.input}
+        onChange={evento => setDataInicio(evento.target.value)}
+        value={dataInicio}
+        required />
+      <input
+        type="time"
+        name="horaInicio"
+        className={style.input}
+        onChange={evento => setHoraInicio(evento.target.value)}
+        value={horaInicio}
+        required />
+    </div>
 
-      <label>Data de término</label>
-      <div className={style.inputContainer}>
-        <input 
-          type="date" 
-          name="dataFim"
-          className={style.input}
-          onChange={evento => setDataFim(evento.target.value)} 
-          value={dataFim}
-          required />
-        <input 
-          type="time" 
-          name="horaFim"
-          className={style.input}
-          onChange={evento => setHoraFim(evento.target.value)} 
-          value={horaFim}
-          required />
-      </div>
+    <label>Data de término</label>
+    <div className={style.inputContainer}>
+      <input
+        type="date"
+        name="dataFim"
+        className={style.input}
+        onChange={evento => setDataFim(evento.target.value)}
+        value={dataFim}
+        required />
+      <input
+        type="time"
+        name="horaFim"
+        className={style.input}
+        onChange={evento => setHoraFim(evento.target.value)}
+        value={horaFim}
+        required />
+    </div>
 
     <button className={style.botao}>
       Salvar
---
+++ formações/formacao_react_com_typescript/event-tracker/src/state/atom.ts
@@ -0,0 +1,24 @@
+import { atom } from "recoil";
+import { IEvento } from "../interfaces/IEvento";
+
+
+
+export const eventListState = atom<IEvento[]>({
+    key: 'eventListState',
+    default: [
+        {
+            "descricao": "Estudar React",
+            "inicio": new Date("2022-01-15T09:00"),
+            "fim": new Date("2022-01-15T13:00"),
+            "completo": false,
+            "id": 1642342747
+        },
+        {
+            "descricao": "Estudar Recoil",
+            "inicio": new Date("2022-01-16T09:00"),
+            "fim": new Date("2022-01-16T11:00"),
+            "completo": false,
+            "id": 1642342959
+        }
+    ]
+})
