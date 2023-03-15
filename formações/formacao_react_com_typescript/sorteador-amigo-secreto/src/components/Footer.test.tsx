e8087f1926d50df50c269f1972f3eec221fc043f
valvanogabryel <valvanogabryel@gmail.com>
2023-03-15 12:14:24 -0300

    /

--- formações/formacao_react_com_typescript/sorteador-amigo-secreto/src/components/Footer.test.tsx
+++ formações/formacao_react_com_typescript/sorteador-amigo-secreto/src/components/Footer.test.tsx
@@ -1,8 +1,27 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { RecoilRoot } from "recoil";
+import { useParticipantsList } from "../state/hooks/useParticipantsList";
 import Footer from "./Footer";
 
+jest.mock('../state/hooks/useParticipantsList', () => {
+  return {
+    useParticipantsList: jest.fn()
+  }
+});
+
+const mockNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => {
+  return {
+    useNavigate: () => mockNavigate
+  }
+});
+
 describe('quando não há participantes o suficiente', () => {
+  beforeEach(() => {
+    (useParticipantsList as jest.Mock).mockReturnValue([]);
+  })
+
   test('a brincadeira não pode ser iniciada', () => {
     render(
       <RecoilRoot>
@@ -17,6 +36,12 @@ describe('quando não há participantes o suficiente', () => {
 });
 
 describe('quando há participantes o suficiente', () => {
+  // const once = 1;
+
+  beforeEach(() => {
+    (useParticipantsList as jest.Mock).mockReturnValue(['Gabryel', 'Fellype', 'José']);
+  })
+
   test('a brincadeira pode ser iniciada', () => {
     render(
       <RecoilRoot>
@@ -27,6 +52,19 @@ describe('quando há participantes o suficiente', () => {
     const button = screen.getByRole('button');
 
     expect(button).not.toBeDisabled();
+  });
+
+  test('a bricadeira foi iniciada', () => {
+    render(
+      <RecoilRoot>
+        <Footer />
+      </RecoilRoot>
+    );
+
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+
+    expect(mockNavigate).toHaveBeenCalledTimes(1);
   });
 });
 
--- formações/formacao_react_com_typescript/sorteador-amigo-secreto/src/components/Footer.tsx
+++ formações/formacao_react_com_typescript/sorteador-amigo-secreto/src/components/Footer.tsx
@@ -1,11 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import { useParticipantsList } from "../state/hooks/useParticipantsList";
 
 const Footer = () => {
   const participants = useParticipantsList();
 
+  const navigate = useNavigate();
+
+  const start = () => {
+    navigate('/sorteio');
+  }
+
   return (
     <footer>
-      <button disabled={participants.length < 3}>Iniciar brincadeira</button>
+      <button
+        disabled={participants.length < 3}
+        onClick={start}
+      >
+        Iniciar brincadeira
+      </button>
     </footer>
   );
 }
