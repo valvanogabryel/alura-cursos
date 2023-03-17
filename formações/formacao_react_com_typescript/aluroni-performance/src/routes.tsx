47099ffcfe652a8ec940fcdd01402cc54d8459bc
valvanogabryel <valvanogabryel@gmail.com>
2023-03-17 13:06:37 -0300

    ;

--- formações/formacao_react_com_typescript/aluroni-performance/src/routes.tsx
+++ formações/formacao_react_com_typescript/aluroni-performance/src/routes.tsx
@@ -1,34 +1,39 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Banner from 'components/Banner';
 import HeaderMenu from 'components/HeaderMenu';
-import Home from 'pages/Home';
-import Menu from 'pages/Menu';
-import About from 'pages/About';
 import Footer from 'components/Footer';
-import NotFound from 'pages/NotFound';
-import Dish from 'pages/Dish';
-import Admin from 'components/Admin';
+
+const Banner = lazy(() => import('components/Banner'));
+const Home = lazy(() => import('pages/Home'));
+const Menu = lazy(() => import('pages/Menu'));
+const Dish = lazy(() => import('pages/Dish'));
+const About = lazy(() => import('pages/About'));
+const NotFound = lazy(() => import('pages/NotFound'));
+const Admin = lazy(() => import('components/Admin'));
+
 
 const AppRouter = () => {
-    return (
-        <main className='container'>
-            <BrowserRouter>
-                <HeaderMenu />
-                <Routes>
-                    <Route path='/' element={<Banner />}>
-                        <Route index element={<Home />} />
-                        <Route path='cardapio' element={<Menu />} />
-                        <Route path='sobre' element={<About />} />
-                    </Route>
-                    <Route path='admin/:user' element={<Admin />} />
-                    <Route path='prato/:id' element={<Dish />} />
-                    <Route path='*' element={<NotFound />} />
-                </Routes>
-                <Footer />
-            </BrowserRouter>
-        </main>
-    );
+  return (
+    <main className='container'>
+      <BrowserRouter>
+        <HeaderMenu />
+        <Suspense fallback={<p>Carregando</p>}>
+          <Routes>
+            <Route path='/' element={<Banner />}>
+              <Route index element={<Home />} />
+              <Route path='cardapio' element={<Menu />} />
+              <Route path='sobre' element={<About />} />
+            </Route>
+            <Route path='admin/:user' element={<Admin />} />
+            <Route path='prato/:id' element={<Dish />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </Suspense>
+        <Footer />
+      </BrowserRouter>
+    </main>
+  );
 };
 
 export default AppRouter;
