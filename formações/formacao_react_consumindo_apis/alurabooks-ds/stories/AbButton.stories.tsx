766f279ecc5c387d78b7e6b43474f50a55f1d295
valvanogabryel <valvanogabryel@gmail.com>
2023-03-20 12:05:26 -0300

    Update AbButton.stories.tsx

--- formações/formacao_react_consumindo_apis/alurabooks-ds/stories/AbButton.stories.tsx
+++ formações/formacao_react_consumindo_apis/alurabooks-ds/stories/AbButton.stories.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-
 import { AbButton } from '../src';
-
-import { ComponentMeta } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 
 export default {
+  title: 'Components/AbButton',
+  component: AbButton
 
-} as ComponentMeta<typeof AbButton>
+} as ComponentMeta<typeof AbButton>;
+
+const Template: ComponentStory<typeof AbButton> = () => <AbButton />;
+
+export const Primary = Template.bind({});
