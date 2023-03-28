a044b00a06d6d84247442ea5649ff0d5e769a6b7
valvanogabryel <valvanogabryel@gmail.com>
2023-03-28 10:20:02 -0300

    ;/

--- react_com_tailwind/alura_newsletter/src/App.tsx
+++ react_com_tailwind/alura_newsletter/src/App.tsx
@@ -1,10 +1,133 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 
-const App = () => {
+const App: React.FC = () => {
+  const [prevScrollPosition, setPrevScrollPosition] = useState(0);
+  const [scrollDirection, setScrollDirection] = useState<'up' | 'down'>('');
+
+  const handleScroll = () => {
+    const currentScrollPosition = window.pageYOffset;
+    const isScrollingUp = currentScrollPosition > prevScrollPosition;
+
+    setScrollDirection(isScrollingUp ? 'up' : 'down');
+    console.log(isScrollingUp);
+    setPrevScrollPosition(currentScrollPosition);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='h-screen flex justify-center items-center'>
-      <Header />
-    </div>
+    <>
+      <Header scrollDir={scrollDirection} />
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+      <section>
+        <p className="text-2xl text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit natus recusandae officia, minus nulla dignissimos esse necessitatibus accusantium asperiores quis officiis maxime repudiandae voluptate quisquam consectetur vel, saepe fugit placeat.</p>
+      </section>
+    </>
   );
 };
 
--- react_com_tailwind/alura_newsletter/src/components/Header/index.tsx
+++ react_com_tailwind/alura_newsletter/src/components/Header/index.tsx
@@ -1,13 +1,25 @@
-const Header = () => {
+import { MoonIcon } from '@heroicons/react/24/solid';
+import { SunIcon } from '@heroicons/react/24/solid';
+
+interface HeaderProps {
+  scrollDir: 'up' | 'down';
+}
+
+const Header: React.FC<HeaderProps> = ({ scrollDir }) => {
+  const headerClasses = `header transition transform duration-200 ease-in-out ${scrollDir === 'up' ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0'}`;
+
   return (
-    <header className=''>
-      <span>Olá, usuário!</span>
-      <h1>Alura Newsletter</h1>
+    <header className={headerClasses}>
+      <span className="">Olá, usuário!</span>
+      <h1 className="text-2xl">Alura Newsletter</h1>
       <input
         type="checkbox"
         name="theme-changer"
         id="theme-changer"
+        className="hidden sm:block"
       />
+      <MoonIcon className='h-8 cursor-pointer hidden sm:block' />
+      <SunIcon className='h-8 cursor-pointer hidden sm:block' />
     </header>
   );
 }
