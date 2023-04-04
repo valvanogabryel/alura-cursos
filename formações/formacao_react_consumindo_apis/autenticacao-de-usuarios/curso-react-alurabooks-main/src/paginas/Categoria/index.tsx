830118e5bb48ce413e9c1bd96741fb1be30bf97d
valvanogabryel <valvanogabryel@gmail.com>
2023-04-04 13:24:38 -0300

    ;

--- formações/formacao_react_consumindo_apis/autenticacao-de-usuarios/curso-react-alurabooks-main/src/paginas/Categoria/index.tsx
+++ formações/formacao_react_consumindo_apis/autenticacao-de-usuarios/curso-react-alurabooks-main/src/paginas/Categoria/index.tsx
@@ -1,13 +1,44 @@
 import { useParams } from "react-router-dom";
 import TituloPrincipal from "../../componentes/TituloPrincipal";
+import { useEffect, useState } from "react";
+import { ICategoria } from "../../interfaces/ICategoria";
+import http from "../../http";
+import Loader from "../../componentes/Loader";
 
 const Categoria = () => {
+  const [categoria, setCategoria] = useState<ICategoria>();
+  const [carregando, setCarregando] = useState<boolean>(true);
+
   const { slug } = useParams();
 
+  // useEffect(() => {
+  //   http.get<ICategoria[]>('categorias', {
+  //     params: {
+  //       slug
+  //     }
+  //   })
+  //     .then(res => {
+  //       setCategoria(res.data[0]);
+  //     });
+  // }, [slug]);
+
+  useEffect(() => {
+    setCarregando(true);
+    http.get<ICategoria[]>(`categorias?slug=${slug}`)
+      .then(res => {
+        setCategoria(res.data[0]);
+        setCarregando(false);
+      })
+  }, [slug]);
+
+  if (carregando) return <Loader />
 
   return (
     <section>
-      <TituloPrincipal>{slug}</TituloPrincipal>
+      <TituloPrincipal>{categoria?.nome}</TituloPrincipal>
+      {
+
+      }
     </section>
   );
 }
