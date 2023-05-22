782ad02bcf3662c400dc0a3747b15fb608a74dfc
valvanogabryel <valvanogabryel@gmail.com>
2023-05-22 15:35:10 -0300

    /

---
+++ formações/formacao_nextjs/alura-cases/src/app/faq/page.tsx
@@ -0,0 +1,10 @@
+const FaqPage = () => {
+  return (
+    <div>
+      <h1>Alura Cases - Perguntas frequentes</h1>
+      <a href="/">Ir para a home</a>
+    </div>
+  )
+}
+
+export default FaqPage;
---
+++ formações/formacao_nextjs/alura-cases/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Alura cases - Home</h1>
+      <a href="/faq">Ir para a FAQ</a>
+    </div>
+  )
+}
