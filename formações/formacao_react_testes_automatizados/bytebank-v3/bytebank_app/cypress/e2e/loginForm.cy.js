cbff7a508dc770af130b6a58fe8397d085ad7d10
valvanogabryel <valvanogabryel@gmail.com>
2023-05-25 10:23:01 -0300

    Create loginForm.cy.js

---
+++ formações/formacao_react_testes_automatizados/bytebank-v3/bytebank_app/cypress/e2e/loginForm.cy.js
@@ -0,0 +1,20 @@
+describe('Login form', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('should not allow an invalid email', () => {
+    cy.getByData('botao-login').click();
+    cy.getByData('email-input').type('gabryelval@gmail');
+    cy.getByData('senha-input').type('gabryelval1');
+    cy.getByData('botao-enviar').click();
+    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido');
+  });
+
+  it('should not allow the input to be empty', () => {
+    cy.getByData('botao-login').click();
+    cy.getByData('senha-input').type('gabryelval1');
+    cy.getByData('botao-enviar').click();
+    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório');
+  });
+});
