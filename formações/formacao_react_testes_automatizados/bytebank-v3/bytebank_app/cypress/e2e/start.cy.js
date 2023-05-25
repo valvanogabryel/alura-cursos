eb7f3082512f23aa1996cff5d643ab3332f75c5c
valvanogabryel <valvanogabryel@gmail.com>
2023-05-25 10:05:13 -0300

    Update start.cy.js

--- formações/formacao_react_testes_automatizados/bytebank-v3/bytebank_app/cypress/e2e/start.cy.js
+++ formações/formacao_react_testes_automatizados/bytebank-v3/bytebank_app/cypress/e2e/start.cy.js
@@ -4,8 +4,6 @@ describe('homepage', () => {
   });
 
   it('should render correctly the text in "advantages" section', () => {
-    // cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!');
-
     cy.verifyText('titulo-principal', 'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!');
   });
 })
