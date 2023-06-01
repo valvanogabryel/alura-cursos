5481c78859b47e16be035a040abb4acb9851419f
gabryel <valvanogabryel@gmail.com>
2023-06-01 10:52:13 -0300

    ;

--- formações/formacao_react_testes_automatizados/bytebank-v3/bytebank_app/cypress/e2e/pages.cy.js
+++ formações/formacao_react_testes_automatizados/bytebank-v3/bytebank_app/cypress/e2e/pages.cy.js
@@ -1,5 +1,19 @@
 describe('multiple pages', () => {
-  it('should have multiple pages', () => {
+  it('should access the cards page', () => {
     cy.visit('/');
+
+    cy.getByData('botao-login').click();
+    cy.getByData('email-input').type('valvanogabryel@gmail.com');
+    cy.getByData('senha-input').type('123');
+    cy.getByData('botao-enviar').click();
+
+    cy.location('pathname').should('eq', '/home');
+
+    cy.getByData('app-home').find('a').eq(1).click();
+    cy.getByData('titulo-cartoes')
+      .should('exist')
+      .and('have.text', 'Meus cartões');
+
+    cy.location('pathname').should('eq', '/home/cartoes');
   });
 });
