fb0ab9deb01c7f299c9bb1462facd70494c36e5d
gabryel <valvanogabryel@gmail.com>
2023-06-09 14:23:16 -0300

    Update api-user.cy.js

--- formações/formacao_react_testes_automatizados/bytebank-v4/cypress_v2_main/cypress/e2e/api/api-user.cy.js
+++ formações/formacao_react_testes_automatizados/bytebank-v4/cypress_v2_main/cypress/e2e/api/api-user.cy.js
@@ -8,4 +8,53 @@ describe('performing requests to API', () => {
       });
     });
   });
+
+  context('GET /users/:userId', () => {
+    it('should return a single user', () => {
+      cy.request({
+        method: 'GET',
+        url: 'http://localhost:8000/users/40a41438-84a6-4b4d-ae1d-7f1713d0a9fe',
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('nome');
+      });
+    });
+
+    it('should return an error when the user is invalid', () => {
+      cy.request({
+        method: 'GET',
+        url: 'http://localhost:8000/users/40a41438-84a6',
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(404);
+        expect(response.body).to.be.eq('Not Found');
+      });
+    });
+  });
+
+  context('interceptating network requests', () => {
+    it('should intercept the POST users/login', () => {
+      cy.intercept({
+        method: 'POST',
+        url: 'users/login',
+      }).as('loginRequest');
+      cy.login('neilton@alura.com', '123456');
+      cy.wait('@loginRequest').then((interception) => {
+        interception.response = {
+          statusCode: 200,
+          body: {
+            success: true,
+            message: 'Login bem sucedido!',
+          },
+        };
+      });
+
+      cy.visit('/home');
+
+      cy.getByData('titulo-boas-vindas').should(
+        'contain.text',
+        'Bem vindo de volta!'
+      );
+    });
+  });
 });
