0c41d3f75c2c09d32e5bc00291313bc53d12cbea
gabryel <valvanogabryel@gmail.com>
2023-06-28 16:02:25 -0300

    Update index.tsx

--- formações/formacao_react_com_typescript/voll-2/voll-parte-2-main/web/src/pages/SignUp/index.tsx
+++ formações/formacao_react_com_typescript/voll-2/voll-parte-2-main/web/src/pages/SignUp/index.tsx
@@ -1,19 +1,63 @@
 import { Step, StepLabel, Stepper } from "@mui/material";
 import logo from "./logo.png";
+import styled from "styled-components";
+import { useState } from "react";
+
+interface CustomProps {
+  color: string;
+}
+
+const Image = styled.img`
+  padding: 2em 0;
+`;
+
+const CustomStep = styled.div<CustomProps>`
+  background-color: ${({ color }) => color};
+  height: 1rem;
+  width: 1rem;
+  border-radius: 50%;
+`;
 
 export default function SignUp() {
+  const [activeStep, setActiveStep] = useState(0);
+
   return (
     <>
       <div>
-        <img src={logo} alt="Logo da Voll" />
-        <Stepper>
+        <Image src={logo} alt="Logo da Voll" />
+        <Stepper activeStep={activeStep}>
           <Step>
-            <StepLabel />
+            <StepLabel
+              StepIconComponent={(props) => (
+                <CustomStep color={props.active ? "lightblue" : "lightgray"} />
+              )}
+            />
           </Step>
           <Step>
-            <StepLabel />
+            <StepLabel
+              StepIconComponent={(props) => (
+                <CustomStep color={props.active ? "lightblue" : "lightgray"} />
+              )}
+            />
           </Step>
         </Stepper>
+
+        {activeStep === 1 && (
+          // PASSO 2 DO FORMULÁRIO
+          <div>
+            <h1>cu</h1>
+          </div>
+        )}
+
+        <button
+          onClick={() => {
+            if (activeStep === 0) {
+              setActiveStep((previousStep) => previousStep + 1);
+            }
+          }}
+        >
+          Next
+        </button>
       </div>
     </>
   );
