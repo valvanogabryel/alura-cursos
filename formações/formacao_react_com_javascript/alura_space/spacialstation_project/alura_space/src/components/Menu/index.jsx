83cc546f2f8cd5bc0e76fa4c8d8d4eeb6524325c
gabryel <valvanogabryel@gmail.com>
2023-07-10 18:10:42 -0300

    ;

--- formações/formacao_react_com_javascript/alura_space/spacialstation_project/alura_space/src/components/Menu/index.jsx
+++ formações/formacao_react_com_javascript/alura_space/spacialstation_project/alura_space/src/components/Menu/index.jsx
@@ -1,30 +1,19 @@
-import React from 'react';
-// import { home, mostLiked, mostViewed, hot, surpriseMe } from './icons';
-import icons from './icons.json';
+import React from "react";
+import icons from "./icons.json";
 
-
-import styles from './Menu.module.scss';
+import styles from "./Menu.module.scss";
 
 export const Menu = () => {
-    return (
-        <nav className={styles.menu}>
-            <ul className={styles.menu__lista}>
-                {
-                    icons.map(icon =>
-                    (
-                        <li key={icon.id}
-                            className={styles.menu__item}>
-                            <img src={icon.path}
-                                alt={icon.name}
-                            />
-                            <a href={icon.link}>
-                                {icon.name}
-                            </a>
-                        </li>
-                    ))
-                }
-            </ul>
-        </nav>
-    )
-}
-
+  return (
+    <nav className={styles.menu}>
+      <ul className={styles.menu__lista}>
+        {icons.map((icon) => (
+          <li key={icon.id} className={styles.menu__item}>
+            <img src={icon.path} alt={icon.name} />
+            <a href={icon.link}>{icon.name}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
--- formações/formacao_react_com_javascript/alura_space/spacialstation_project/alura_space/src/pages/homepage/index.jsx
+++ formações/formacao_react_com_javascript/alura_space/spacialstation_project/alura_space/src/pages/homepage/index.jsx
@@ -1,30 +1,27 @@
-import { Fragment } from "react";
-// components
 import Header from "components/Header";
 import { Menu } from "components/Menu";
 import Banner from "components/Banner";
 import Gallery from "components/Gallery";
 import Populars from "components/Populars";
 import Footer from "components/Footer";
-// end of components
 
-import styles from './homepage.module.scss';
+import styles from "./homepage.module.scss";
 
 export default function Homepage() {
-    return (
-        <Fragment>
-            <Header />
-            <main>
-                <section className={styles.main}>
-                    <Menu />
-                    <Banner />
-                </section>
-                <div className={styles.gallery}>
-                    <Gallery />
-                    <Populars />
-                </div>
-            </main>
-            <Footer />
-        </Fragment>
-    )
+  return (
+    <>
+      <Header />
+      <main>
+        <section className={styles.main}>
+          <Menu />
+          <Banner />
+        </section>
+        <div className={styles.gallery}>
+          <Gallery />
+          <Populars />
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
