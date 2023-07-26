4e3cf38a969d842c61a5de73e27ae96d730b948a
gabryel <valvanogabryel@gmail.com>
2023-07-26 18:10:43 -0300

    ;

--- formações/formacao_node/node1/index.js
+++ formações/formacao_node/node1/index.js
@@ -1,1 +1,36 @@
-console.log('olá mundo');
+import fs from "fs";
+import chalk from "chalk";
+
+function fileCatcher(path) {
+  fs.promises
+    .readFile(path, "utf-8")
+    .then((data) => {
+      console.log(chalk.green(data));
+    })
+    .catch((err) => {
+      if (err)
+        throw new Error(
+          chalk.bold.bgRedBright(
+            `${err.code}: não há arquivo no diretório especificado.`
+          )
+        );
+    });
+}
+
+// function fileCatcher(path) {
+//   const encoding = "utf-8";
+
+//   const fileContent = fs.readFile(path, encoding, (err, data) => {
+//     if (err)
+//       throw new Error(
+//         chalk.bold.bgRedBright(
+//           `${err.code}: Não há arquivo no diretório especificado.`
+//         )
+//       );
+//     console.log(chalk.green(data));
+//   });
+
+//   return fileContent;
+// }
+
+fileCatcher("./arquivos/texto.md");
