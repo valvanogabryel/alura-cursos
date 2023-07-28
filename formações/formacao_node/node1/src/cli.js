faa78f7f374b626c9989b0339b0fcd2b982305cf
gabryel <valvanogabryel@gmail.com>
2023-07-28 11:18:16 -0300

    Update cli.js

--- formações/formacao_node/node1/src/cli.js
+++ formações/formacao_node/node1/src/cli.js
@@ -1,14 +1,23 @@
 import chalk from "chalk";
+import fs from "fs";
 import fileCatcher, { handleError } from "./index.js";
 
 const path = process.argv;
 
-async function processText(path) {
-  const results = await fileCatcher(path[2]);
+async function processText(args) {
+  const path = args[2];
 
-  if (!results) handleError("Não há links no arquivo.");
+  if (fs.lstatSync(path).isFile()) {
+    const results = await fileCatcher(path);
 
-  console.log(chalk.bold.yellow("Lista de links:"), results);
+    if (!results) handleError("Não há links no arquivo.");
+
+    console.log(chalk.bold.yellow("Lista de links:"), results);
+  } else if (fs.lstatSync(path).isDirectory()) {
+    const files = await fs.promises.readdir(path);
+
+    console.log(files);
+  }
 }
 
 processText(path);
